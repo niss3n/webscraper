c9cfcb486538145c53efe872e81b79e7099d6682
Brad Nissen <bradanissen@gmail.com>
2020-09-27 10:36:51 -0700

    using client_id of app reg instead of object_id of app reg

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,8 +112,8 @@ resource "azurerm_key_vault" "kv" {
   }
   
    access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "${data.azurerm_client_config.current.object_id}"
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.client_id
 	
 
     key_permissions = [
