c114b926da27fa7a5211b939c4b18a4cb67f9061
Brad Nissen <bradanissen@gmail.com>
2020-10-17 18:03:17 -0700

    Mod name of KV

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_function_app" "fa" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
-  name                        = "bnissenkeyvaulttwo"
+  name                        = "bnissenkeyvaultt"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
