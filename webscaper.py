0025ef63d905d1b0bd5e4b5129ce2daf0b985480
Paul Sin <jiyoungsin97@gmail.com>
2020-10-22 23:59:35 -0400

    Integration of Sendgrid.

--- webscaper.py
+++ webscaper.py
@@ -3,6 +3,9 @@
 
 from bs4 import BeautifulSoup
 import requests
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 url = 'https://www.bowflex.com/selecttech/552/100131.html'
 response = requests.get(url, timeout=5)
@@ -14,8 +17,20 @@ currentContent = str(currentContent).replace("\"","'")
 
 soldOutContent = "<button class='black add-to-cart add-to-cart-disabled' disabled='disabled' id='add-to-cart' title='Select ' type='button' value='Select '><span>Add to Cart</span><issvghelper icon='arrow-right'></issvghelper></button>"
 
-if soldOutContent != currentContent:
-    print('there has been a change, send an email!')
+if soldOutContent == currentContent:
+    message = Mail(
+        from_email='',
+        to_emails='',
+        subject='Weight Prices Update',
+        html_content='<strong>There has been a change in the price of Weights</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
 else:
     print('still sold out')
 
